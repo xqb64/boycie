346c3d896eb828d3db1f55d02bd5d6192f630514
adder46 <dedmauz69@gmail.com>
2021-06-18 23:11:46 +0200

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,54 @@
+import trio
+
+COMMANDER = b'adder!~adder@user/adder'
+
+NETWORK = 'irc.libera.chat'
+PORT = 6667
+NICK = 'Boycie'
+REAL_NAME = 'Boycie'
+USER_NAME = 'Boycie'
+
+async def say(stream, target: str, message: str) -> None:
+    print("got message: {}".format(repr(message)))
+    await send(stream, "PRIVMSG %s :%s" % (target, message))
+
+async def join(stream, channel: str) -> None:
+    await send(stream, "JOIN %s" % channel)
+
+async def send(stream, message: str) -> None:
+    message = message.encode('UTF-8')
+    await stream.send_all(message + b'\r\n')
+
+async def main() -> None:
+    stream = await trio.open_tcp_stream(NETWORK, PORT)
+
+    await send(stream, "NICK %s" % NICK)
+    await send(stream, "USER %s * 0: %s" % (USER_NAME, REAL_NAME))
+
+    while True:
+        msg = await stream.receive_some()
+        print(msg)
+
+        if msg == b'':
+            break
+
+        if msg.startswith(b'PING'):
+            reply = msg.decode('UTF-8').strip()
+            reply = reply.replace('PING', 'PONG')
+            await send(stream, reply)
+        
+        if msg.startswith(b':%s' % COMMANDER):
+            msg = msg.decode('UTF-8')
+            msg_from_commander = msg.split(':')
+            command = msg_from_commander[-1]
+
+            if command.startswith('!say'):
+                cmd = command.strip().split(' ')
+                target, speech = cmd[1], cmd[2:]
+                await say(stream, target, ' '.join(speech))
+
+            elif command.startswith('!join'):
+                await join(stream, command[6:])
+
+
+trio.run(main)
